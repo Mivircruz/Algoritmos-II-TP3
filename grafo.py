8c168a36e6c1dbd9741675d33e9442ecc92b4c85
Milagros Cruz <mivircruz@gmail.com>
2018-12-19 13:21:27 -0300

    grafo diccionario de diccionarios

--- grafo.py
+++ grafo.py
@@ -1,16 +1,24 @@
-#!/usr/bin/python3
-
-
 
 class Vertice(object):
 	def __init__(self, ciudad, codigo, latitud, longitud):
 		self.vertice = {codigo: (ciudad, latitud, longitud)}
+		self.adyacentes = {}
 
 	def obtener_codigo(self):
 		return self.vertice[codigo]
 		
 	def obtener_ciudad(self):
 		return self.vertice.keys()
+
+	def agregar_adyacente(self, codigo, codigo_ady, tiempo, precio, cant_vuelos):
+		self.adyacentes[codigo] = {codigo_ady : (tiempo, precio, cant_vuelos)}
+
+	def son_adyacentes(self, codigo_ady):
+		if codigo_ady in self.adyacentes:
+			return  True
+		return False
+	def obtener_adyacentes(self):
+		return self.adyacentes
 		
 
 '''''
@@ -37,50 +45,42 @@ aristas = {
 
 '''''
 
+#Grafo implementado como diccionario de diccionarios
+
 class Grafo(object):
+
 	def __init__(self):
-		self.vertices = []
-		self.aristas = {}
+		self.vertices = {}
 
 	def agregar_vertice(self, ciudad, codigo, latitud, longitud):
 		if codigo not in self.vertices:
-			self.vertices.append(Vertice(ciudad, codigo, latidud, longitud))
+			self.vertices[codigo] = Vertice(ciudad,codigo,latitud,longitud)
 
 	
 	def agregar_arista(self, aeropuerto1, aeropuerto2, tiempo, precio, cant_vuelos):
-		if aeropuerto1 not in self.aristas:
-			self.aristas[aeropuerto1] = {}
-
-		self.aristas[aeropuerto1][aeropuerto2] = (tiempo, precio, cant_vuelos)
-
-		if aeropuerto2 not in self.aristas:
-			self.aristas[aeropuerto2] = {}
-
-		self.aristas[aeropuerto2][aeropuerto1] = (tiempo, precio, cant_vuelos)
-		
+		self.vertices.get(aeropuerto1).agregar_adyacente(self.vertices[aeropuerto1],aeropuerto1,  aeropuerto2, tiempo, precio, cant_vuelos)
+		self.vertices.get(aeropuerto2).agregar_adyacente(self.vertices[aeropuerto2],aeropuerto2,  aeropuerto1, tiempo, precio, cant_vuelos)
 
 	def estan_conectados(self, codigo1, codigo2):
-		if codigo1 in self.vertices.codigos() and codigo2 in self.vertices.codigos():
-			return True
-		return False
+		return self.vertices.get(codigo1).son_adyacentes(self.vertices[codigo1],codigo2)
 
 
 	def obtener_vertice(self, codigo):
 		if len(self.vertices) == 0:
 			return None
-		
-		for i in range(0, len(self.vertices)):
-			if self.codigos[i] == codigo:
-				return  self.vertices[i]
+		return self.vertices.get(codigo)
 
 
 	def obtener_adyacentes(self, codigo):
-		if codigo in self.codigos:
-			return Vertice.obtener_adyacentes(self.vertices[codigo])
+
+		return self.vertices.get(codigo).obtener_adyacentes(self.vertices[codigo])
 
 
 	def obtener_largo(self):
 		return len(self.vertices)
-		
+
+
+	#NO SÉ QUÉ ODNA ESTE MÉTODO:
+
 	def __iter__(self):
 		return iter(self.vertices.values)
