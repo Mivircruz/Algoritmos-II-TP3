e5e54ca6c272d3f9556d622ad34e4f7690400190
nicodebole <43222401+nicodebole@users.noreply.github.com>
2018-12-20 16:22:28 -0300

    Update vertice.py

--- vertice.py
+++ vertice.py
@@ -13,28 +13,16 @@ class Vertice(object):
     def obtener_ciudad(self):
         return self.ciudad
 
-    def agregar_adyacente(self, codigo, tiempo, precio, cant_vuelos):
-        self.adyacentes[codigo] = [tiempo, precio, cant_vuelos]
+    def agregar_adyacente(self, ciudad, tiempo, precio, cant_vuelos):
+        self.adyacentes[ciudad] = [tiempo, precio, cant_vuelos]
 
-    def obtener_adyacente(self, codigo):
-        return self.adyacentes[codigo]
+    def obtener_adyacente(self, ciudad):
+        return self.adyacentes[ciudad]
 
-    def son_adyacentes(self, codigo_ady):
-        if codigo_ady in self.adyacentes:
+    def son_adyacentes(self, ciudad_ady):
+        if ciudad_ady in self.adyacentes:
             return True
         return False
 
     def obtener_adyacentes(self):
         return self.adyacentes
-
-    def obtener_tiempo(self, codigo):
-        datos_conexion = self.adyacentes.get(codigo)
-        return datos_conexion[0]
-
-    def obtener_precio(self, codigo):
-        datos_conexion = self.adyacentes.get(codigo)
-        return datos_conexion[1]
-
-    def obtener_cant_vuelos(self, codigo):
-        datos_conexion = self.adyacentes.get(codigo)
-        return datos_conexion[2]
