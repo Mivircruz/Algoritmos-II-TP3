034e3908ec9e42f927c45a65a111587482f2ed3c
nicodebole <43222401+nicodebole@users.noreply.github.com>
2018-12-20 16:22:07 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -34,34 +34,57 @@ class Grafo(object):
 
 	def agregar_vertice(self, ciudad, codigo, latitud, longitud):
 
-		self.vertices[codigo] = vertice.Vertice(ciudad,codigo,latitud,longitud)
+		self.vertices[ciudad] = vertice.Vertice(ciudad, codigo, latitud, longitud)
 
 	
-	def agregar_arista(self, aeropuerto1, aeropuerto2, tiempo, precio, cant_vuelos):
+	def agregar_arista(self, ciudad1, ciudad2, tiempo, precio, cant_vuelos):
 
-		vertice.agregar_adyacente(self.vertices[aeropuerto1],aeropuerto1,  aeropuerto2, tiempo, precio, cant_vuelos)
-		vertice.agregar_adyacente(self.vertices[aeropuerto2],aeropuerto2,  aeropuerto1, tiempo, precio, cant_vuelos)
+		self.vertices[ciudad1].agregar_adyacente(ciudad2, tiempo, precio, cant_vuelos)
+		self.vertices[ciudad2].agregar_adyacente(ciudad1, tiempo, precio, cant_vuelos)
 
-	def estan_conectados(self, codigo1, codigo2):
-		return vertice.son_adyacentes(self.vertices[codigo1],codigo2)
+	def estan_conectados(self, ciudad1, ciudad2):
+		return vertice.son_adyacentes(self.vertices[ciudad1], ciudad2)
 
 
-	def obtener_vertice(self, codigo):
-		return self.vertices.get(codigo)
+	def obtener_vertice(self, ciudad):
+		return self.vertices.get(ciudad)
 
 	def obtener_todos_vertices(self):
 		return self.vertices
 
 
-	def obtener_adyacentes(self, codigo):
-		return vertice.obtener_adyacentes(self.vertices[codigo])
+	def obtener_adyacentes(self, ciudad):
+		return self.vertices[ciudad].obtener_adyacentes()
 
+	def obtener_tiempo(self, ciudad1, ciudad2):
+		if ciudad1 in self.vertices.keys():
+			if ciudad2 in self.vertices[ciudad1].adyacentes.keys():
+				datos_conexion = self.vertices[ciudad1].adyacentes[ciudad2]
+				return datos_conexion[0]
+
+		return None
+
+	def obtener_precio(self, ciudad1, ciudad2):
+		if ciudad1 in self.vertices.keys():
+			if ciudad2 in self.vertices[ciudad1].adyacentes.keys():
+				datos_conexion = self.vertices[ciudad1].adyacentes[ciudad2]
+				return datos_conexion[1]
+
+		return None
+
+	def obtener_cant_vuelos(self, ciudad1, ciudad2):
+		if ciudad1 in self.vertices.keys():
+			if ciudad2 in self.vertices[ciudad1].adyacentes.keys():
+				datos_conexion = self.vertices[ciudad1].adyacentes[ciudad2]
+				return datos_conexion[2]
+
+		return None
 
 	def obtener_largo(self):
 		return len(self.vertices)
 
-	def pertenece(self, codigo):
-		if codigo not in self.vertices:
+	def pertenece(self, ciudad):
+		if ciudad not in self.vertices:
 			return False
 		return True
 
