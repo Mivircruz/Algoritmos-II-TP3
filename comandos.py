8f390f604d6ecb28bf4fa9b900e3b886c2f42bc8
Milagros Cruz <mivircruz@gmail.com>
2018-12-20 04:27:47 -0300

    mas comandos

--- comandos.py
+++ comandos.py
@@ -5,15 +5,37 @@ import funciones
 def listar_operaciones():
     print("nueva_aerolinea")
     print("caminos_mas")
+    print("camino_escalas")
 
 comandos = {"listar_operaciones": listar_operaciones,
             "caminos_mas" : camino_mas}
 
 def camino_mas(modo, origen, destino, grafo):
-    
-    if modo == "rapido":
-        padres = funciones.camino_mas_rapido(grafo, origen, destino)
-        print(origen, "->")
-        for key, values in padres:
-            print(padres[key], "->")
-        print(destino)
+
+    padres = funciones.camino_mas_rapido(grafo, origen, destino, modo)
+    print(origen, "->")
+    for key, values in padres:
+        print(padres[key], "->")
+    print(destino)
+
+def camino_escalas(origen, destino, grafo):
+
+    recorrido = funciones.camino_minimo(grafo, origen, destino)
+    print(origen, "->")
+    for key, velues in recorrido:
+        print(recorrido[key], "->")
+    print(destino)
+
+def centralidad_aprox(grafo, n):
+
+    cent = funciones.centralidad(grafo)
+    i = 0
+
+    for key, values in cent:
+        if i == n:
+            return
+        print(cent[key])
+        if i < n-1
+            print(',')
+
+
--- funciones.py
+++ funciones.py
@@ -42,12 +42,9 @@ def bfs(grafo, origen):
 				orden[w] = orden[v] + 1
 				cola.append(w)
 
-	return padres, orden
+	return padres,orden
 
-def camino_mas_rapido(grafo, origen, destino):
-
-	if not Grafo.pertenece(grafo, origen) or not Grafo.pertenece(grafo,origen):
-		return False
+def camino_minimo(grafo, origen, destino):
 
 	distancia = {}
 	padres = {}
@@ -64,7 +61,8 @@ def camino_mas_rapido(grafo, origen, destino):
 	while heap_tiempo:
 		v = heapq.heappop(heap_tiempo)
 		visitados.add(v[1])
-		if v[1] == destino:
+		ciudad = vertice.obtener_ciudad(grafo.obtener_vertice(grafo, key))
+		if ciudad == destino:
 			return padres
 
 		vertice_actual = grafo.obtener_vertice(grafo, v[1])
@@ -72,27 +70,36 @@ def camino_mas_rapido(grafo, origen, destino):
 		for key, value in vertice.obener_adyacentes(vertice_actual):
 
 			if key not in visitados:
-				if distancia[v[1]] + Vertice.obtener_tiempo(vertice_actual, key) < distancia[key]:
-					distancia[key] = distancia[v[1]] + vertice.obtener_tiempo(vertice_actual, key)
+				if distancia[v[1]] + vertice.obtener_cant_vuelos(vertice_actual, key) < distancia[key]:
+					distancia[key] = distancia[v[1]] + vertice.obtener_cant_vuelos(vertice_actual, key)
 					padres[key] = v[1]
 					heapq.heappush(heap_tiempo, (dist[key], key))
 
 	return padres
 
-def camino_mas_barato(grafo, origen, destino):
+def camino_mas_modo(grafo, origen, destino, modo):
 
 	visitados = set()
 	visitados.append(origen)
-	heap_precios = []
+	heap = []
 	camino = []
 
 
 	for key, value in vertice.obtener_adyacentes(grafo.obtener_vertice(grafo, origen)):
-		heapq.heappush(heap_precios, (vertice.obtener_precio(grafo.obtener_vertice(grafo, origen), key))
+		if modo == "barato"
+			a_guardar =  vertice.obtener_precio(grafo.obtener_vertice(grafo, origen))
+		else
+			a_guardar = vertice.obtener_tiempo(grafo.obtener_vertice(grafo, origen))
+		heapq.heappush(heap, (a_guardar, key))
 
 	while heap_precios:
 		v = heapq.heappop(heap_precios)
 		camino.append(v[1])
+
+		ciudad = vertice.obtener_ciudad(grafo.obtener_vertice(grafo, key))
+		if ciudad == destino:
+			return camino
+
 		if v[1] in visitados:
 			continue
 		visitados.append(v[1])
@@ -102,6 +109,43 @@ def camino_mas_barato(grafo, origen, destino):
 		for key,values in vertice.obtener_adyacentes(vertice_actual):
 
 			if key not in visitados:
-				heapq.heappush(heap_precios, (vertice.obtener_precio(grafo.obtener_vertice(grafo, v[1]), key)))
+				if modo == "barato"
+					a_guardar = vertice.obtener_precio(grafo.obtener_vertice(grafo, v[1]))
+				else
+					a_guardar = vertice.obtener_tiempo(grafo.obtener_vertice(grafo, v[1]))
+				heapq.heappush(heap_precios, (a_guardar, key)))
 
 	return camino
+
+def centralidad(grafo):
+
+	cent = {}
+
+	for key, values in grafo.obtener_todos_vertices(grafo):
+		cent[key] = 0
+
+	for key1, values in grafo.obtener_todos_vertices(grafo):
+		padres_y_dist = bfs(grafo, vertice.obtener_ciudad(grafo.obtener_vertice(grafo, key1)))
+		cent_aux = {}
+		for key2, values in grafo.obtener_todos_vertices(grafo):
+			cent_aux[key2] = 0
+
+		#Filtra infinitos
+		for key2, dist in padres_y_dist[1]:
+			if dist == math.inf:
+				padres_y_dist[1].pop(dist)
+
+
+#HCER FUNCION ORDENAR_VERTICES!!!!!
+		vertices_ordenados = ordenar_vertices(grafo, dist)
+
+		for w in vertices_ordenados:
+			cent_aux[padres_y_dist[0][w]] += 1
+			cent_aux[padres_y_dist[0][w]] += cent_aux[w]
+
+		for key2, values in grafo.obtener_todos_vertices(grafo):
+			if key1 == key2:
+				continue
+			cent[key2] += cent_aux[key2]
+	return cent
+
